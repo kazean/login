d78e2544da2883a214c98f37c70935b5f08efcca
kazean <yeogunna@gmail.com>
2022-02-09 14:22:54 +0900

    cookie session

---
+++ src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,65 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 세션관리 만들기
+ */
+@Component
+public class SessionManager {
+    private static final String SESSION_COOKIE_NAME = "mySessionId";
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    /**
+     * 세션 생성
+     */
+    public void createSession(HttpServletResponse response, Object value){
+        String sessionId = UUID.randomUUID().toString();
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+
+        sessionStore.put(sessionId, value);
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 세션조회
+     */
+    public Object getSession(HttpServletRequest request){
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if(sessionCookie == null){
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request){
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if(sessionCookie != null){
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if(request.getCookies() == null){
+            return null;
+        }
+
+        return Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+
+}
